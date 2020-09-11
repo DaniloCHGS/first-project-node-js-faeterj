2f04dcfb13e93054aace515d4d6a0a0491b6825d
Danilo Chagas <danilochagasbm@gmail.com>
2020-09-11 12:08:43 -0300

    Adicinando o vetor

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const server = express();
-server.get('/users/:id', (req, resp)=>{
+const users = ['Danilo', 'Pedro', 'Brendo'];server.get('/users/:id', (req, resp)=>{
     const {id} = req.params;
-    resp.json({msg: `Bem vindo ${id}`})
+    resp.json({msg: `Bem vindo ${users[id-1]}`});
 });
 server.listen(3000, '127.0.0.1', ()=>{
     console.log('Server runing');

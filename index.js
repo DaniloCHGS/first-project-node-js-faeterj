e183c8f7ca2ed203d4d80592424e0d4bcca50f05
Danilo Chagas <danilochagasbm@gmail.com>
2020-09-15 12:20:00 -0300

    CRUD e GitIgnore

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 const express = require('express');
 const server = express();
-const users = ['Danilo', 'Pedro', 'Brendo'];server.get('/users/:id', (req, resp)=>{
+server.use(express.json());
+const users = ['Danilo', 'Pedro', 'Brendo'];
+
+//Read - lista
+server.get('/users/:id', (req, resp)=>{
     const {id} = req.params;
-    resp.json({msg: `Bem vindo ${users[id-1]}`});
+    resp.json({msg: `Usuário: ${users[id-1]}`});
 });
+server.get('/users', (req, resp)=>{
+    return resp.json(users);
+});
+
+//Create - insere
+server.post('/users', (req, resp)=>{
+    const {name} = req.body;
+    users.push(name);
+    return resp.json(users);
+});
+
+//Put - edita
+server.put('/users/:index', (req, resp)=>{
+    const {index} = req.params;
+    const {name} = req.body;
+    users[index] = name;
+    return resp.json(users);
+});
+
+//Delete - exclui
+server.delete('/users/:index', (req, resp)=>{
+    const {index} = req.params;
+    users.splice(index, 1);
+    return resp.json(users);
+})
+
 server.listen(3000, '127.0.0.1', ()=>{
     console.log('Server runing');
 });
